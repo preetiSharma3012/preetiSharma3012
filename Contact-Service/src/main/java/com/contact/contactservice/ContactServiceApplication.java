1c4c8039a97094b099e06b6204f2d41473113855
sharm <sharm@LAPTOP-MB4LRIGV>
2021-12-27 16:23:02 +0530

    service discovery added

--- Contact-Service/src/main/java/com/contact/contactservice/ContactServiceApplication.java
+++ Contact-Service/src/main/java/com/contact/contactservice/ContactServiceApplication.java
@@ -2,12 +2,17 @@ package com.contact.contactservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ContactServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContactServiceApplication.class, args);
+		 
 	}
 
 }
--- User-Service/src/main/java/com/user/userservice/controller/UserServiceController.java
+++ User-Service/src/main/java/com/user/userservice/controller/UserServiceController.java
@@ -1,10 +1,13 @@
 package com.user.userservice.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.user.userservice.entity.User;
 import com.user.userservice.service.UserService;
@@ -16,9 +19,17 @@ public class UserServiceController {
 	@Autowired
 	private UserService userservice;
 	
+	@Autowired
+	private RestTemplate resttemplate;
+	
 	@GetMapping("/{userid}")
 	public User getUser(@PathVariable("userid") Long userid)
 	{
-		return  userservice.getUser(userid);
+		User user= userservice.getUser(userid);
+		List contacts = resttemplate.getForObject("http://contact-service/contact/user/" + userid, List.class);
+		
+		user.setContacts(contacts);
+			
+		return user;
 	}
 }
